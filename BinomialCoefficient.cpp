00d5d47c3696169d8e939ff5f18e01f6db759d22
Capobmb <82255007+Capobmb@users.noreply.github.com>
2023-06-10 15:15:13 +0900

    BinomialCoefficient 仕様変更

--- BinomialCoefficient.cpp
+++ BinomialCoefficient.cpp
@@ -3,44 +3,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// ac-library 依存バージョン
-#include <atcoder/all>
-template<size_t MOD = 1000000007, size_t MAX = 1000010>
+
+template<long long MOD = 998244353>
 struct Bicoef {
-    using mint = atcoder::static_modint<MOD>;
-    Bicoef() {
-        fac[0]  = fac[1]  = mint::raw(1);
-        finv[0] = finv[1] = mint::raw(1);
-                  inv[1]  = mint::raw(1);
-        for (size_t i = 2; i < MAX; i++) {
-            fac[i]  = fac[i-1] * i;
-            inv[i]  = MOD - inv[MOD%i] * (MOD/i) ;
-            finv[i] = finv[i-1] * inv[i];
+    std::vector<long long> fac, finv, inv;
+    Bicoef(int Max) : fac(Max), finv(Max), inv(Max) {
+        assert(Max < MOD);
+         fac[0] =  fac[1] = 1;
+        finv[0] = finv[1] = 1;
+                   inv[1] = 1;
+        for (long long i = 2; i < Max; i++) {
+            fac[i]  = fac[i-1] * i % MOD;
+            inv[i]  = MOD - inv[MOD%i] * (MOD/i) % MOD;
+            if(inv[i] == MOD) inv[i] = 0;
+            finv[i] = finv[i-1] * inv[i] % MOD;
         }
     }
-    mint com(int n, int k) const {
+    inline long long com(int n, int k) const {
         if (n < k) return 0;
         if (n < 0 || k < 0) return 0;
-        return fac[n] * finv[k] * finv[n-k];
+        return fac[n] * finv[k] % MOD * finv[n-k] % MOD;
     }
-    mint perm(int n, int k) const {
+    inline long long perm(int n, int k) const {
         if (n < k) return 0;
         if (n < 0 || k < 0) return 0;
-        return fac[n] * finv[n-k];
+        return fac[n] * finv[n-k] % MOD;
     }
-    mint fact(int n) const { return fac[n]; }
-    
-    mint fac[MAX];
-    mint finv[MAX];
-    mint inv[MAX];
+    inline long long fact(int n) const { return fac[n]; }
 };
-// Bicoef<998244353, 2000010> bf;
+// Bicoef<998244353> bf(n);
 
 // --------------------------------------
 
-
-// ac-library 非依存バージョン
-// Note : constexprコンストラクタは MAX <= 2^18 = 262144 ~ 2.6 x 10^5 が必要
+// Constexpr version
+// Note: Due to constexpr restriction, MAX <= 2^18 = 262144 ~ 2.6 x 10^5
 template<long long MOD = 998244353, size_t MAX = 262144> // MAX must be 
 struct Bicoef {    
     constexpr Bicoef() : fac(), finv(), inv() {
